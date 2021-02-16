c547eb2509ff7f50c121154ec7634d2c5d79eef6
Saleh Alibrahim <s.albrahim@outlook.com>
2021-02-17 01:11:46 +0300

    feat: Add files model

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,24 +1,34 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
-import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File.FileForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note.NoteForm;
+import com.udacity.jwdnd.course1.cloudstorage.security.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.service.FileService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/files")
+@RequestMapping("/file")
 public class FileController {
 
-    private final FileMapper fileMapper;
+    private final FileService fileService;
+    private final UserService userService;
 
-
-    public FileController(FileMapper fileMapper) {
-        this.fileMapper = fileMapper;
+    public FileController(FileService fileService, UserService userService) {
+        this.fileService = fileService;
+        this.userService = userService;
     }
 
     @PostMapping("/upload")
-    public String fileUpload(){
-        return "";
+    public String uploadFile(@ModelAttribute("newFile") FileForm newFile){
+
+        System.out.println(newFile.getFile());
+
+        return "redirect:/home";
     }
 
 
